0c8f0de81bb843dbcb43a6d452a739f15bce8e53
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-06-18 22:07:33 -0700

    corrected month from appreviation to entire month

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 ///set moment variable & print to home page
 var todayDate = moment();
-console.log(todayDate.format('dddd, '+'MMM Do YYYY'));
+console.log(todayDate.format('dddd, '+'MMMM Do YYYY'));
 
-$("#currentDay").text(todayDate.format('dddd, '+'MMM Do YYYY'));
+$("#currentDay").text(todayDate.format('dddd, '+'MMMM Do YYYY'));
