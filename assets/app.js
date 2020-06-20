7dfe57b6dd5ede9c40da43e496de76110a001835
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-06-20 15:03:31 -0700

    Added bootstrap input-groups to html to assist with local storage of input data

--- assets/app.js
+++ assets/app.js
@@ -4,10 +4,30 @@ var todayDate = moment();
 //pull today's date, connect to id in html header, and format
 $("#currentDay").text(todayDate.format('dddd, '+'MMMM Do YYYY'));
 
-//set variables for save buttons
-var task;
-var hourInfo;
+ //set variable for 12-hour time format
+ var currentTime = moment().format("h");
+  //set variable for 24-hour time format
+ var milTime = moment().format("H");
 
+
+ for (var hour = 9; hour <= 17; hour++) {
+    var index = hour;
+    var blockTime = $("#" + index).data("military");
+  
+
+  // set color attribute for blocks according to current time
+  if (blockTime == milTime) {
+    $("#" + index).css({"background-color": "#ff6961", "color": "white"});
+  } 
+  else if (blockTime < milTime) {
+    $("#" + index).css({"background-color": "#d3d3d3", "color": "white"});
+  }
+  else if (blockTime > milTime) {
+    $("#" + index).css({"background-color": "#77dd77", "color": "white"});
+  }
+  
+ }
+    
 
 //button to clear local and refresh the page
 $("#clear").click(function() {
@@ -15,67 +35,43 @@ $("#clear").click(function() {
     location.reload()
 });
 
-//Update field color based on current hour
-var currentHour = moment().hours();
+  //set variable to get submitted tasks out of local storage for each time block
+  var nineAM = localStorage.getItem("9");
+  $("#9").val(nineAM);
 
-var id = parseInt($(this).attr("id"));
-if (id === currentHour) {
-  $(this).attr("class", "row time-block present");
-} else if (id < currentHour) {
-  $(this).attr("class", "row time-block past");
-} else if (id > currentHour) {
-  $(this).attr("class", "row time-block future");
-}
+  var tenAM = localStorage.getItem("10");
+  $("#10").val(tenAM);
+
+  var elevenAM = localStorage.getItem("11");
+  $("#11").val(elevenAM);
+
+  var noon = localStorage.getItem("12");
+  $("#12").val(noon);
+
+  var onePM = localStorage.getItem("13");
+  $("#13").val(onePM);
+
+  var twoPM = localStorage.getItem("14");
+  $("#14").val(twoPM);
+
+  var threePM = localStorage.getItem("15");
+  $("#15").val(threePM);
+
+  var fourPM = localStorage.getItem("16");
+  $("#16").val(fourPM);
+
+  var fivePM = localStorage.getItem("17");
+  $("#17").val(fivePM);
+
+//function to save task into local storage
+
+$(".saveBtn").on("click", function() {
+    var messageId = $(this).data("time");
+    console.log($(this).data("time"));
+    $("#" + messageId).val();
+    localStorage.setItem(messageId, $("#" + messageId).val());
+  })
 
 
-//function to make the save button work
-$(".saveBtn").click(function() {
-    task = $(this).siblings(".input").val();
-    console.log(task);
-    hourInfo = $(this).siblings(".hour").text();
-    console.log(hourInfo);
-    localStorage.setItem(hourInfo, JSON.stringify(task));
-});
 
-//function to save tasks. 
-$(document).ready(function(){
-    renderTask();
-});
 
-function renderTask() {
-    var saveTask9 = JSON.parse(localStorage.getItem("9:00 am"));
-    $("#9").val("");
-    $("#9").val(saveTask9);
-
-    var saveTask10 = JSON.parse(localStorage.getItem("10:00 am"));
-    $("#10").val("");
-    $("#10").val(saveTask10);
-
-    var saveTask11 = JSON.parse(localStorage.getItem("11:00 am"));
-    $("#11").val("");
-    $("#11").val(saveTask11);
-
-    var saveTask12 = JSON.parse(localStorage.getItem("12:00 pm"));
-    $("#12").val("");
-    $("#12").val(saveTask12);
-
-    var saveTask1 = JSON.parse(localStorage.getItem("1:00 pm"));
-    $("#13").val("");
-    $("#13").val(saveTask1);
-
-    var saveTask2 = JSON.parse(localStorage.getItem("2:00 pm"));
-    $("#14").val("");
-    $("#14").val(saveTask2);
-
-    var saveTask3 = JSON.parse(localStorage.getItem("3:00 pm"));
-    $("#15").val("");
-    $("#15").val(saveTask3);
-
-    var saveTask4 = JSON.parse(localStorage.getItem("4:00 pm"));
-    $("#16").val("");
-    $("#16").val(saveTask4);
-
-    var saveTask5 = JSON.parse(localStorage.getItem("5:00 pm"));
-    $("#17").val("");
-    $("#17").val(saveTask5);
-};
