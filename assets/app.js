cd75e84a6f32e62db68977378f7f66e29d07aa2d
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-12-23 15:02:52 -0800

    Added loop for dynamic pulling from Local Storage and Octocat waving

--- assets/app.js
+++ assets/app.js
@@ -1,61 +1,54 @@
-
-//Load date on jumbotron
+// Load date on jumbotron
 let dt = new Date();
-let todaysDate =  `${dt.getMonth()+1} / ${dt.getDate()} / ${dt.getFullYear()}`;
-$("#currentDay").text(todaysDate);
+let todaysDate = `${dt.getMonth() + 1} / ${dt.getDate()} / ${dt.getFullYear()}`;
+$("#current-day").text(todaysDate);
 
-//Check current time every 15 seconds
+// Check current time every 15 seconds
 setInterval(checkTime(), 15000);
 
 // Activate save button
-$(".saveBtn").on("click", function() {
-    // Save task and time
-    let task = $(this).siblings(".description").val();
-    let time = $(this).parent().attr("id");
+$(".saveBtn").on("click", function () {
 
-    //Save in local storage
-    localStorage.setItem(time,task);
-  })
+  // Set variables using DOM relationships
+  let time = "#" + $(this).parent().attr("id");
+  let task = $(this).siblings(".description").val();
 
-  // When checking time, change the color of the text area
-function checkTime(){
-  let currentHour = dt.getHours()
-  
-  $(".time-block").each(function(){
+  // Save in local storage with key as "hour-X" and value as description field
+  localStorage.setItem(time, task);
+})
+
+// When checking time, change the color of the text area
+function checkTime() {
+  let currentHour = dt.getHours();
+
+  $(".time-block").each(function () {
     // parseInt takes the id of the time block and converts the string into a number for validation
-    let blockTime = parseInt($(this).attr("id").split("-")[1])
-  
-  // set color attribute for description according to current time using bootstrap classes
-  if (blockTime < currentHour){
-    $(this.children[1]).addClass("bg-secondary text-white");
-  } else if
-  (blockTime === currentHour) {
-  $(this.children[1]).removeClass("bg-secondary text-white");
-  $(this.children[1]).addClass("bg-success text-white");
-  } else if (blockTime > currentHour) {
-    $(this.children[1]).removeClass("bg-success text-white");
-    $(this.children[1]).addClass("bg-warning");
-  } 
-  })
+    let blockTime = parseInt($(this).attr("id").split("-")[1]);
+
+    // Set color attribute for description according to current time using Bootswatch classes
+    if (blockTime < currentHour) {
+      $(this.children[1]).addClass("bg-secondary text-white border-primary");
+    } else if (blockTime === currentHour) {
+      $(this.children[1]).removeClass("bg-secondary text-white");
+      $(this.children[1]).addClass("bg-success text-white border-success");
+    } else if (blockTime > currentHour) {
+      $(this.children[1]).removeClass("bg-success text-white");
+      $(this.children[1]).addClass("bg-primary text-white");
+    }
+
+    // Check localStorage for data
+    // if there is data, load on page
+    for (let i = 0; i < localStorage.length; i++) {
+      let blockTimeId = localStorage.key(i);
+      $(blockTimeId)
+        .children(".description")
+        .val(localStorage.getItem(localStorage.key(i)));
+    }
+  });
 }
- 
-//button to clear local and refresh the page
-$("#clearBTN").click(function() {
-    localStorage.clear();
-    location.reload()
+
+//button to clear local storage for a new day
+$("#clearBTN").click(function () {
+  localStorage.clear();
+  location.reload();
 });
-
-   // load any saved data from localStorage
-   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-   $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-   $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-   $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
-
-
-
